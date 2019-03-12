39b8c9d7af6a6c8b0fd17080e3aff212e5f3a418
rizkihilmi <48423060+rizkihilmi@users.noreply.github.com>
2019-03-12 10:42:28 +0700

    merubah tombol add menjadi modal

--- team.php
+++ team.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-4">
-		 <a href="index.php?page=add-team" class="btn btn-custom btn-rounded w-md waves-effect waves-light mb-4"><i class="fa fa-plus-circle"></i> Add Team </a>
+		 <button class="btn btn-custom btn-rounded w-md waves-effect waves-light mb-4" data-toggle="modal" data-target=".bs-example-modal-md"><i class="fa fa-plus-circle"></i> Add Team </button
 	</div>
 </div>
 <div class="row">
@@ -32,4 +32,45 @@
 		</div>
 	</div>
 	<?php } ?>
+</div>
+<!-- MODAL  -->
+<div class="modal fade bs-example-modal-md" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-md">
+		<div class="modal-content">
+			<form action="team-action.php?action=save" method="post" enctype="multipart/form-data" id="form">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span></button>
+					<h4 class="modal-title" id="myModalLabel">Add Team</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+					<label>Name</label>
+					<input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+				</div>
+				<div class="form-group">
+					<label>Job</label>
+					<input type="text" class="form-control" id="job" name="job" placeholder="Enter Job">
+				</div>
+				<div class="form-group">
+					<label>Email</label>
+					<input type="email" class="form-control" id="email"  name="email" placeholder="Enter Email">
+				</div>
+				<div class="form-group">
+					<label>No Handphone</label>
+					<input type="text" class="form-control" id="nohp" name="nohp" placeholder="Enter Number">
+				</div>
+				<div class="form-group">
+				<label>Photo <span class="mandatory">*</span></label>				
+				<div style="margin-bottom:10px;">
+					<input type="file" name="img" id="img" required style="margin-bottom:5px;" />
+				</div>
+				</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
